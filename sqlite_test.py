c2cddea0b1c02a43682fc3c2c432689059d5e006
inesmassa <inesurvivor@gmail.com>
2026-04-30 18:16:17 +0200

    s

--- sqlite_test.py
+++ sqlite_test.py
@@ -5,13 +5,18 @@ connexion = sql.connect("films.db")
 connexion.execute('''
             CREATE TABLE IF NOT EXISTS films (
             id INTEGER PRIMARY KEY,
-            titre TEXT,
+            titre TEXT UNIQUE,
             annee INTEGER,
             note REAL
             )
 ''')
 connexion.commit()
-connexion.execute('INSERT INTO films (titre, annee, note) VALUES (?, ?, ?)', ('Devil wears Prada', 2010, 10))
-connexion.execute('INSERT INTO films VALUES (?, ?, ?, ?)', ('Moonlight', 2019, 9.9))
-connexion.execute('INSERT INTO TABLE films (titre, annee, note) VALUES (?, ?, ?)', ('Hamilton', 2016, 8.5))
+connexion.execute('INSERT OR IGNORE INTO films (titre, annee, note) VALUES (?, ?, ?)', ('Devil wears Prada', 2010, 10))
+connexion.execute('INSERT OR IGNORE INTO films (titre, annee, note) VALUES (?, ?, ?)', ('Moonlight', 2019, 9.9))
+connexion.execute('INSERT OR IGNORE INTO films (titre, annee, note) VALUES (?, ?, ?)', ('Hamilton', 2016, 8.5))
 connexion.commit()
+
+films = connexion.execute('SELECT * FROM films').fetchall()
+print(type(films))
+for film in films:
+    print(film[0], film[1], film[2], film[3])
