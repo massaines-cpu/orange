200c638504711f5c959878374ea22bc0e32e01ae
inesmassa <inesurvivor@gmail.com>
2026-05-01 13:55:54 +0200

    sup

--- main.py
+++ main.py
@@ -23,7 +23,36 @@ def les_films():
 @app.get('/films/{id}')
 def recup_film(id: int):
     connexion = sql.connect("films.db")
-    film = connexion.execute('SELECT * FROM films WHERE id = ?', (id,)).fetchall()
+    film = connexion.execute('SELECT * FROM films WHERE id = ?', (id,)).fetchone()
+    connexion.close()
 
     if film is None:
         return {'message': 'film non trouvé'}
+
+    return {
+        'id': film[0],
+        'titre': film[1],
+        'annee': film[2],
+        'note': film[3]
+    }
+
+@app.post('/films')
+def create_film(titre:str, annee:int, note:float):
+    connexion = sql.connect("films.db")
+    cursor = connexion.execute(
+        'INSERT INTO films (titre, annee, note) VALUES (?, ?, ?)', (titre, annee, note)
+    )
+    connexion.commit()
+    connexion.close()
+
+    return {'message': 'ok', 'id': cursor.lastrowid}
+
+@app.put('/films/{id}')
+def modif_film(id:int, titre:str, annee:int, note:float):
+    connexion = sql.connect("films.db")
+    cursor = connexion.execute('UPDATE films SET titre = ?, annee = ?, note = ? WHERE id = ?',
+                               (titre, annee, note, id)
+                               )
+    connexion.commit()
+    connexion.close()
+    return {'message': 'ok maj'}
